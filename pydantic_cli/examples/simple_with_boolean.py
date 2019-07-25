cba8930a9ffa8ff46105a1523c98dbe32c65251c
Michael <michael.kocher@me.com>
2019-07-24 19:06:34 -0700

    Add boolean arg example

---
+++ pydantic_cli/examples/simple_with_boolean.py
@@ -0,0 +1,23 @@
+"""
+Simple Example of using Pydantic to generate a CLI
+
+This example will generate a CLI tool with 1 positional argument and one optional boolean arg
+"""
+from pydantic import BaseModel
+
+from pydantic_cli import run_and_exit
+
+
+class Options(BaseModel):
+    input_file: str
+    run_training: bool = False
+
+
+def example_runner(opts: Options) -> int:
+    print(f"Mock example running with {opts}")
+    print(f"Fields {opts.fields}")
+    return 0
+
+
+if __name__ == "__main__":
+    run_and_exit(Options, example_runner, description=__doc__, version='0.1.0')
