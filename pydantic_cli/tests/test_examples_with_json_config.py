7a69bc60053186f5a01019929a93a0710f9b8532
Michael <michael.kocher@me.com>
2020-06-25 00:22:12 -0700

    Add smoke test for JSON loading

---
+++ pydantic_cli/tests/test_examples_with_json_config.py
@@ -0,0 +1,35 @@
+from . import _TestUtil, TestConfig
+
+import json
+from tempfile import NamedTemporaryFile
+from pydantic_cli.examples.simple_with_json_config import Opts, runner
+
+
+class TestExample(_TestUtil):
+
+    CONFIG = TestConfig(model=Opts, runner=runner)
+
+    def _util(self, d, more_args):
+        with NamedTemporaryFile(mode="w", delete=True) as f:
+            json.dump(d, f)
+            f.flush()
+            f.name
+            args = ["--json-config", str(f.name)] + more_args
+            self.run_config(args)
+
+    def test_simple_json(self):
+        opt = Opts(hdf_file="/path/to/file.hdf5",
+                   max_records=12,
+                   min_filter_score=1.024,
+                   alpha=1.234,
+                   beta=9.854)
+        self._util(opt.dict(), [])
+
+    def test_simple_partial_json(self):
+        d = dict(
+                   max_records=12,
+                   min_filter_score=1.024,
+                   alpha=1.234,
+                   beta=9.854)
+
+        self._util(d, ['--hdf_file', "/path/to/file.hdf5"])
