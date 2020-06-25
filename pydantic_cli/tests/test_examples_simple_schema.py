3fde873d01a30d26b36b5c9c1bc6aa209c80c6be
Michael <michael.kocher@me.com>
2020-06-25 00:21:57 -0700

    Fix tests

--- pydantic_cli/tests/test_examples_simple_schema.py
+++ pydantic_cli/tests/test_examples_simple_schema.py
@@ -1,24 +1,20 @@
-from unittest import TestCase
-
 from pydantic_cli import to_runner
 from pydantic_cli.examples.simple_schema import Options, example_runner
 
+from . import _TestUtil, TestConfig
 
-class TestExamples(TestCase):
 
-    def _run_with_args(self, args):
-        f = to_runner(Options, example_runner)
-        exit_code = f(args)
-        self.assertEqual(exit_code, 0)
+class TestExamples(_TestUtil):
+    CONFIG = TestConfig(model=Options, runner=example_runner)
 
     def test_01(self):
         args = '-f /path/to/file.txt --max_records 1234 -s 1.234 --max_filter_score 10.234'
-        self._run_with_args(args.split())
+        self.run_config(args.split())
 
     def test_02(self):
         args = '-f /path/to/file.txt -m 1234 -s 1.234 -S 10.234'
-        self._run_with_args(args.split())
+        self.run_config(args.split())
 
     def test_03(self):
-        self._run_with_args(['-f', '/path/to/file.txt', '-s', '1.234'])
+        self.run_config(['-f', '/path/to/file.txt', '-s', '1.234'])
 
--- pydantic_cli/tests/test_examples_simple_with_boolean.py
+++ pydantic_cli/tests/test_examples_simple_with_boolean.py
@@ -1,16 +1,12 @@
-from unittest import TestCase
-
 from pydantic_cli import to_runner
 from pydantic_cli.examples.simple_with_boolean import Options, example_runner
 
+from . import _TestUtil, TestConfig
 
-class TestExamples(TestCase):
 
-    def _run_with_args(self, args):
-        f = to_runner(Options, example_runner)
-        exit_code = f(args)
-        self.assertEqual(exit_code, 0)
+class TestExamples(_TestUtil):
+    CONFIG = TestConfig(model=Options, runner=example_runner)
 
     def test_simple_01(self):
-        self._run_with_args(['/path/to/file.txt', '--run_training', 'True'])
+        self.run_config(["--input_file", "/path/to/file.txt", '--run_training', 'True'])
 
