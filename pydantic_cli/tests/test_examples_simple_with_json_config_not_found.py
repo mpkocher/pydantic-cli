d50fcc14152491526d15ae95e41eb04464944695
Michael <michael.kocher@me.com>
2020-08-02 13:21:15 -0700

    Add new tests

---
+++ pydantic_cli/tests/test_examples_simple_with_json_config_not_found.py
@@ -0,0 +1,26 @@
+from . import _TestHarness, TestConfig
+
+from pydantic_cli.examples.simple_with_json_config_not_found import (
+    Options,
+    example_runner,
+)
+
+
+class TestExamples(_TestHarness[Options]):
+
+    CONFIG = TestConfig(Options, example_runner)
+
+    def test_simple_01(self):
+        self.run_config(["--input_file", "/path/to/file.txt", "--max_record", "1234"])
+
+    def test_simple_02(self):
+        self.run_config(
+            [
+                "--input_file",
+                "/path/to/file.txt",
+                "--max_record",
+                "1234",
+                "--json-config",
+                "/bad/path/file.json",
+            ]
+        )
