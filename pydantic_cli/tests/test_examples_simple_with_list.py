8501137a345166d79d6b170e15b75630dc37c468
Michael <michael.kocher@me.com>
2021-08-19 16:38:11 -0700

    Add CI check with black. Update black to 21.7b0

--- pydantic_cli/tests/test_examples_simple_with_list.py
+++ pydantic_cli/tests/test_examples_simple_with_list.py
@@ -8,7 +8,14 @@ class TestExamples(_TestHarness[Options]):
     CONFIG = HarnessConfig(Options, example_runner)
 
     def test_simple_01(self):
-        args = ["--input_file", "/path/to/file.txt", "/and/another/file.txt",
-                "--max_record", "1234",
-                "--filters", "alpha", "beta"]
+        args = [
+            "--input_file",
+            "/path/to/file.txt",
+            "/and/another/file.txt",
+            "--max_record",
+            "1234",
+            "--filters",
+            "alpha",
+            "beta",
+        ]
         self.run_config(args)
